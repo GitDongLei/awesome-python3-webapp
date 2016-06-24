bc98be2a4f2be4fb74d8c5868781b483f78c5ed7
donglei <ldong@mail.ustc.edu.cn>
2016-06-24 18:32:00 +0800

    add the manage_blog_edit.html and signin.html

--- www/apis.py
+++ www/apis.py
@@ -13,23 +13,19 @@ class APIError(Exception):
     '''
     the base APIError which contains error(reqired), data(optional) and message(optional).
     '''
-
     def __init__(self, error, data='', message=''):
         super(APIError, self).__init__(message)
         self.error= error
         self.data = data
         self.message = message
 
-
 class APIValueError(APIError):
     '''
     Indicate the input value has error or invalid. The date specifieds the error field of input form.
     '''
-
     def __init__(self, field, message=''):
         super(APIValueError, self).__init__('value:invalid', field, message)
 
- 
 class APIResourceNotFoundError(APIError):
     '''
     Indicate the resource wat not found. The data specifies the resource name.
--- www/orm.py
+++ www/orm.py
@@ -11,8 +11,6 @@ logging.basicConfig(level=logging.INFO)
 def log(sql,args=()):
     logging.info('SQL: %s' % sql)
 
-
-
 async def create_pool(loop, **kw):
     logging.info('create database connection pool...')
     global __pool
